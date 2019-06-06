eadeb1c5a274f6526b204f1b49ae2742c22ee028
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2019-06-06 16:22:15 +0200

    fixed incorrect plane labels in jhu

--- data-raw/jhu.R
+++ data-raw/jhu.R
@@ -1,21 +1,21 @@
-jhu = rio::import("data-raw/geobrain_JHU.Rda") %>% 
-  mutate(aparc = gsub("ifl", "ilf", aparc),
-         aparc = gsub("sflt", "slft", aparc)) %>%
-  separate(aparc, c("hemi","acronym"), remove = FALSE) %>%
-  mutate(acronym = ifelse(!hemi %in% c("lh","rh"), hemi, acronym),
-         acronym = ifelse(acronym %in% "gm", NA, acronym),
-         acronym = ifelse(id %in% 100:101, "cst", acronym),
-         aparc = ifelse(aparc %in% "gm", NA, aparc),
-         hemi = ifelse(hemi %in% "lh", "left",
-                       ifelse(hemi %in% "rh", "right", "center")),
-         aparc = gsub("\\.", "_", aparc),
-         side = ifelse(id %in% c(200:240), "upper coronal",
-                       ifelse(id %in% c(1:13), "lower coronal","axial")),
-         atlas = "jhu"
-  ) %>%
-  mutate(acronym = ifelse(id %in% c(5,8,217),
-                          "slft", acronym)) %>% 
-  rename(label = aparc)
+# jhu = rio::import("data-raw/geobrain_JHU.Rda") %>% 
+# mutate(aparc = gsub("ifl", "ilf", aparc),
+#        aparc = gsub("sflt", "slft", aparc)) %>%
+#   separate(aparc, c("hemi","acronym"), remove = FALSE) %>%
+#   mutate(acronym = ifelse(!hemi %in% c("lh","rh"), hemi, acronym),
+#          acronym = ifelse(acronym %in% "gm", NA, acronym),
+#          acronym = ifelse(id %in% 100:101, "cst", acronym),
+#          aparc = ifelse(aparc %in% "gm", NA, aparc),
+#          hemi = ifelse(hemi %in% "lh", "left",
+#                        ifelse(hemi %in% "rh", "right", "center")),
+#          aparc = gsub("\\.", "_", aparc),
+#          side = ifelse(id %in% c(200:240), "upper axial",
+#                        ifelse(id %in% c(1:13), "lower axial","coronal")),
+#          atlas = "jhu"
+#   ) %>%
+#   mutate(acronym = ifelse(id %in% c(5,8,217),
+#                           "slft", acronym)) %>% 
+#   rename(label = aparc)
 
 tmp = data.frame(area = c("Anterior thalamic radiation", "Corticospinal tract","Cingulum (cingulate gyrus)",
                           "Cingulum (hippocampus)","Forceps major","Forceps minor",
@@ -24,7 +24,7 @@ tmp = data.frame(area = c("Anterior thalamic radiation", "Corticospinal tract","
                  acronym=c("atr","cst","ccg","cab","fmajor","fminor","ifof","ilf","slf","unc","slft", "csf"))
 
 jhu = jhu %>%
-  left_join(tmp) %>%
+#  left_join(tmp) %>%
   #rename(label=aparc) %>%
   #select(-meas, -piece) %>% 
   select(lat, long, area, hemi, side, acronym, atlas, everything())
@@ -36,18 +36,23 @@ jhu <- jhu %>%
 for(i in 1:nrow(jhu)){
   jhu$pos[[i]] = list(
     stacked = list(),
-      dispersed = list(x = list(breaks = c(7.5, 10.75, 14),
-                                labels = c("upper coronal","axial", "lower coronal")),
-                       y = list(breaks = NULL,
-                                labels = NULL),
-                       labs = list(x = "side",
-                                   y = NULL))
+    dispersed = list(x = list(breaks = c(7.5, 10.75, 14),
+                              labels = c("upper coronal","axial", "lower coronal")),
+                     y = list(breaks = NULL,
+                              labels = NULL),
+                     labs = list(x = "side",
+                                 y = NULL))
   )
 }
 
 jhu <- jhu %>%
   select(one_of(names(ggseg::dkt))) %>% 
-  ggseg:::as_ggseg_atlas()
+  ggseg:::as_ggseg_atlas() %>% 
+  mutate(side = case_when(
+    side == "upper coronal" ~ "upper axial",
+    side == "lower coronal" ~ "lower axial",
+    side == "axial" ~ "coronal"
+  )) 
 
 usethis::use_data(jhu, internal = FALSE, overwrite = TRUE, compress = "xz")
 
