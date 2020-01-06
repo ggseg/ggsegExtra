a648bf35cf875fb96fc2a233c6151ec6cebeceaa
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-01-06 11:43:44 +0100

    removed schafer 200 for checks

--- R/schaefer.R
+++ R/schaefer.R
@@ -1,35 +1,3 @@
-### 2d polygons ----
-
-#' Local-Global 17 Parcellation of the Human Cerebral Cortex
-#'
-#' Polygon data for the 17 resting-state networks of
-#' the Schaefer et al. (2018) networks, in a 200 area parcellation
-#'
-#' @docType data
-#' @name schaefer17_200
-#' @usage data(schaefer17_200)
-#' @family ggseg_atlases
-#'
-#' @keywords datasets
-#'
-#' @references Schaefer et al. (2018) Cereb Cortex. 2018 Sep 1;28(9):3095-3114. doi: 10.1093/cercor/bhx179
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/28981612}{PubMed})
-#'
-#' @format A data.frame
-#' \describe{
-#'   \item{long}{coordinates for the x-axis}
-#'   \item{lat}{coordinates for the y-axis}
-#'   \item{area}{name of network}
-#'   \item{hemi}{name of the hemisphere (left, right)}
-#'   \item{side}{which side to view (medial, lateral)}
-#'   \item{network}{network number (1:7)}
-#'   \item{label}{unique name to each node}
-#'   \item{atlas}{name of the atlas}
-#' }
-#'
-#' @examples
-#' data(schaefer17_200)
-"schaefer17_200"
 
 
 ### 3d meshes ----
