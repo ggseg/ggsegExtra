7560ad300cf9ce972d58f635cb52c3fb0aa969c8
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-02-26 10:59:30 +0100

    testing

--- R/bash-exec.R
+++ R/bash-exec.R
@@ -10,7 +10,7 @@ run_tcl <- function(region, indir, hemisphere, outdir, verbose = TRUE){
                  "; export TD=", outdir, "; ")
   
   
-  tcl_script <- system.file("bash_scripts", "snapshot.tcl", package = "ggsegExtra")
+  tcl_script <- system.file("sh", "snapshot.tcl", package = "ggsegExtra")
   
   fs_cmd <- paste0(freesurfer::get_fs(),
                    "tksurfer")
@@ -34,9 +34,12 @@ run_smooth_labels <- function(lab_file,
                               output, 
                               verbose=TRUE){
   
-  smooth_script <- system.file("bash_scripts", "smooth_labels.sh", package = "ggsegExtra")
+  smooth_script <- system.file("sh", "smooth_labels.sh", package = "ggsegExtra")
+  
+  wb <- paste0("export PATH=", get_wb_command(), ";")
   
   cmd <- paste(
+    wb,
     smooth_script,
     lab_file,
     surf_file,
@@ -46,4 +49,22 @@ run_smooth_labels <- function(lab_file,
   k <- system(cmd, intern=!verbose)
   
   invisible(k)
+}
+
+
+get_wb_command <- function(){
+  PATH <- strsplit(Sys.getenv("PATH"), ":")[[1]]
+  path <- PATH[grepl("workbench", PATH)]
+  
+  if(length(path) == 0){
+    if(grepl("darwin", sessionInfo()$platform)){
+      dirs <- list.dirs("/Applications/workbench", recursive = FALSE)
+      dirs <- dirs[grepl("bin", dirs)]
+      PATH <- c(PATH, dirs)
+    }
+  }
+  
+  PATH <- paste0(PATH, collapse = ":")
+  
+  return(PATH)
 }
