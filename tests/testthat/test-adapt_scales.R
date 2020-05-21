f09b22c5422484a2e84c4695de66870cc532ccde
muschellij2 <muschellij2@gmail.com>
2020-05-21 15:02:36 -0400

    neuroc_release fixes for suggests and extra ggseg atlas packages

--- tests/testthat/test-adapt_scales.R
+++ tests/testthat/test-adapt_scales.R
@@ -1,15 +1,41 @@
 library(dplyr)
-test_that("Check that ggseg:::adapt_scales is working", {
-  expect_is(ggseg::ggseg(atlas = yeo17, mapping=aes(fill=area),adapt_scales = F ) +
-              scale_brain("yeo17"),c("gg","ggplot"))
+testthat::test_that("Check that ggseg:::adapt_scales is working", {
   
-  expect_is(ggseg::ggseg(atlas = yeo7, mapping=aes(color=area),adapt_scales = F ) +
-              scale_color_brain("yeo7"),c("gg","ggplot"))
+  if (requireNamespace("ggsegYeo2011", quietly = TRUE)) {
+    library(ggsegYeo2011)
+    testthat::expect_is(ggseg::ggseg(
+      atlas = yeo17,
+      mapping = aes(fill=area),
+      adapt_scales = FALSE ) +
+        ggseg::scale_brain("yeo17", package = "ggsegYeo2011"),
+      c("gg","ggplot"))
+    
+    testthat::expect_is(ggseg::ggseg(
+      atlas = yeo7, 
+      mapping = aes(color=area),
+      adapt_scales = FALSE ) +
+        ggseg::scale_color_brain("yeo7", 
+                                 package = "ggsegYeo2011"),
+      c("gg","ggplot"))
+  }
   
-  expect_equal(mode(ggseg:::adapt_scales(yeo7 %>% unnest(ggseg))), "list")
-  expect_equal(mode(ggseg:::adapt_scales(yeo17 %>% unnest(ggseg))), "list")
-  expect_equal(mode(ggseg:::adapt_scales(glasser %>% unnest(ggseg))), "list")
-  expect_equal(mode(ggseg:::adapt_scales(tracula %>% unnest(ggseg))), "list")
-  expect_equal(mode(ggseg:::adapt_scales(jhu %>% unnest(ggseg))), "list")
-  expect_equal(mode(ggseg:::adapt_scales(hoCort %>% unnest(ggseg))), "list")
+  atlases = list(
+    "ggsegYeo2011" = "yeo7", 
+    "ggsegYeo2011" = "yeo17", 
+    "ggsegGlasser" = "glasser", 
+    "ggsegTracula" = "tracula", 
+    "ggsegJHU" = "jhu", 
+    "ggsegHO" = "hoCort")
+  for (iatlas in seq_along(atlases)) {
+    atlas = atlases[[iatlas]]
+    atlas_package = names(atlases)[[iatlas]]
+    if (requireNamespace(atlas_package, quietly = TRUE)) {
+      atlas = get(atlas, envir = asNamespace(atlas_package))
+      testthat::expect_equal(
+        mode(ggseg:::adapt_scales(atlas %>% tidyr::unnest(ggseg))), "list")
+    } else {
+      message(paste0("Cannot test ", atlas, 
+                     ", need to install ", atlas_package))
+    }
+  }
 })
