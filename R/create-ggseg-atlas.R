dadb05461902c7ff7d596fa79af1127e4de89a0b
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-11-26 09:51:37 +0100

    fixed error in func example

--- R/create-ggseg-atlas.R
+++ R/create-ggseg-atlas.R
@@ -289,7 +289,7 @@ make_palette_ggseg <- function(ggseg3d_atlas){
 #' @export
 #'
 #' @examples
-#' if(!freesurfer::have_fs()){
+#' if(freesurfer::have_fs()){
 #' 
 #' aseg2 <- make_volumetric_ggseg(label_file =  file.path(subjects_dir, subject, "mri/aseg.mgz"))
 #' 
